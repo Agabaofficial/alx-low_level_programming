2c6588eec38c2352ce76d9f863f2fcabe8dde81a
Agabaofficial <agabaolivier85@gmail.com>
2023-03-17 07:41:01 -0700

    ury

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -4,18 +4,14 @@
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-  char c = 'a';
-
-  while (c <= 'z')
-    {
-      putchar(c);
-      c++;
-    }
-
-  putchar('\n');
-
-  return 0;
+char c = 'a';
+while (c <= 'z')
+{
+putchar(c);
+c++;
+}
+putchar('\n');
+return 0;
 }
