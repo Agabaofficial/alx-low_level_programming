43c0afa510cf82f203cea32dab39d88ff31f9164
Agabaofficial <agabaolivier85@gmail.com>
2023-03-19 03:22:24 -0700

    low level programming

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,30 +1,22 @@
-#include <stdio.h>
-
-/**
-* main - Print combinations of two digit numbers
-*
-* Return: Always 0 (Success)
-*/
 int main(void)
 {
-int tens;
-int ones;
+    int first_digit, second_digit;
 
-for (tens = 0; tens <= 9; tens++)
+    for (first_digit = 0; first_digit <= 8; first_digit++)
     {
-for (ones = tens + 1; ones <= 9; ones++)
-	{
-putchar(tens + '0');
-putchar(ones + '0');
+        for (second_digit = first_digit + 1; second_digit <= 9; second_digit++)
+        {
+            putchar(first_digit + '0');
+            putchar(second_digit + '0');
 
-if (tens < 8)
-	    {
-putchar(',');
-putchar(' ');
-	    }
-	}
+            if (!(first_digit == 8 && second_digit == 9))
+            {
+                putchar(',');
+                putchar(' ');
+            }
+        }
     }
-putchar('\n');
+    putchar('\n');
 
-return (0);
+    return 0;
 }
