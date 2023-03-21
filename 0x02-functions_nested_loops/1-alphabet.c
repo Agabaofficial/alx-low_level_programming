765019f18cef800d111ce677306331b9cca12bac
Agabaofficial <agabaolivier85@gmail.com>
2023-03-21 04:14:30 -0700

    c programs

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,11 +6,11 @@
  */
 void print_alphabet(void)
 {
-  char i;
+char i;
 
-  for (i = 'a'; i <= 'z'; i++)
+for (i = 'a'; i <= 'z'; i++)
     {
-      _putchar(i);
+_putchar(i);
     }
-  _putchar('\n');
+_putchar('\n');
 }
