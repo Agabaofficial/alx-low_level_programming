5061d6e765789e9e07e58e7c9aab346aa0217881
Agabaofficial <agabaolivier85@gmail.com>
2023-04-04 04:50:12 -0700

    pointer that point to pointers

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,21 +1,21 @@
 #include "main.h"
-/**
- *_memset - fill a block of memory with a specific value
- *@s: starting address of memory to be filled
- *@b: the desired value
- *@n: number of bytes to be changed
- *
- *Return: changed array with new value for n bytes
- */
-char *_memset(char *s, char b, unsigned int n)
-{
-  int i = 0;
 
-  while (n > 0)
-    {
-      s[i] = b;
-      i++;
-      n--;
-    }
-  return (s);
+/**
+ * *_memcpy - copies memory area
+ * @dest: destination memory area
+ * @src: memory area to copy from
+ * @n: number of bytes to copy
+ *
+ * Return: pointer to dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	return (dest);
 }
